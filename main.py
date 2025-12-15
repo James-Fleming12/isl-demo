48ee2d846924242f370cc8818bc4b1680f9d9f8d
James Fleming <jamesfleming.sd@gmail.com>
2025-12-15 14:53:27 -0800

    aligned code with original training loop

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
@@ -6,7 +5,9 @@ import torchvision.transforms as T
 
 from PIL import Image
 from diffusers.models import AutoencoderKL
+from datasets import load_dataset
 
+from OmniGenCode.OmniGen.train_helper.data import TrainDataCollator
 from fm_noise_scheduler import FlowMatchEulerDiscreteScheduler
 
 from omni_cust import CustomOmniGen
@@ -15,30 +16,6 @@ from OmniGenCode.OmniGen.utils import vae_encode, vae_encode_list
 from OmniGenCode.OmniGen.train_helper import training_losses
 
 from transformers import Phi3Config
-
-class SimpleTextToImageDataset(Dataset):
-    def __init__(self):
-        self.samples = [
-            ("A white cat resting on a picnic table.", "cat.png"),
-            ("A person walking on a suspension bridge.", "walking.png"),
-        ]
-
-        self.image_dir = Path("/home/james/Research/QLab/isl-demo/OmniGenCode/toy_data/images")
-        self.transform = T.Compose([
-            T.Resize((256, 256)),
-            T.ToTensor(),
-            T.Normalize([0.5]*3, [0.5]*3),
-        ])
-
-    def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, idx):
-        caption, image_file = self.samples[idx]
-        image = Image.open(self.image_dir / image_file).convert("RGB")
-        image = self.transform(image)
-
-        return image, caption
 
 def main():
     batch_size = 2
@@ -51,7 +28,10 @@ def main():
         num_hidden_layers=4,
         num_attention_heads=8,
     )
-    model = CustomOmniGen(config).to(device)
+    model = CustomOmniGen(config)
+    model.to(device)
+    model.llm.config.use_cache = False
+    model.llm.gradient_checkpointing_enable()
     model.train()
 
     processor = OmniGenProcessor.from_pretrained("Shitao/OmniGen-v1")
@@ -59,33 +39,35 @@ def main():
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
-    dataset = SimpleTextToImageDataset()
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    dataset = load_dataset("yzwang/X2I-text-to-image", split="train", streaming=True)
+    collate_fn = TrainDataCollator(
+        pad_token_id=processor.text_tokenizer.eos_token_id,
+        hidden_size=model.llm.config.hidden_size
+    )
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_fn)
 
     for epoch in range(epochs):
-        for batch, (images, captions) in enumerate(dataloader):
-            images = images.to(device)
-            if isinstance(images, list):
-                images_latents = vae_encode_list(vae, images, torch.float32).to(device)
-            else:
-                images_latents = vae_encode(vae, images, torch.float32).to(device)
+        for batch, data in enumerate(dataloader):
+            output_images = data['output_images']
+            with torch.no_grad():
+                output_images = vae.encode(output_images).latent_dist.sample()
+            model_kwargs = dict(
+                input_ids=data['input_ids'],
+                input_img_latents=None,
+                input_image_sizes=data['input_image_sizes'],
+                attention_mask=data['attention_mask'],
+                position_ids=data['position_ids'],
+                padding_latent=data.get('padding_images', None),
+                past_key_values=None,
+                return_past_key_values=False
+            )
 
-            processed = processor(instructions=captions)
-            input_ids = processed["input_ids"].to(device)
-            attention_mask = processed["attention_mask"].to(device)
-            position_ids = processed["position_ids"].to(device)
-            input_image_sizes = {i: [(0, images_latents.shape[1])] for i in range(batch_size)}
-            
-            model_kwargs = dict(input_ids=input_ids, input_img_latents=images_latents, input_image_sizes=input_image_sizes, attention_mask=attention_mask, position_ids=position_ids)
-
-            loss_dict = training_losses(model, images_latents, model_kwargs)
+            loss_dict = training_losses(model, output_images, model_kwargs)
             loss = loss_dict["loss"].mean()
 
-            print(loss)
-
-            break
-        break
-
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
 if __name__=="__main__":
     main()
