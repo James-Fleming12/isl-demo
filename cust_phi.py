346db03e75972e859a8a7abb3973bdb0ab907bed
James Fleming <jamesfleming.sd@gmail.com>
2025-12-26 13:01:03 -0800

    debug

--- cust_phi.py
+++ cust_phi.py
@@ -698,7 +698,6 @@ class BlockPhi3(Phi3PreTrainedModel):
         # add hidden states from the last decoder layer
         # if output_hidden_states:
         #     all_hidden_states += (hidden_states,)
-        all_hidden_states = all_hidden_states[:-1]
 
         next_cache = next_decoder_cache if use_cache else None
         if return_legacy_cache:
