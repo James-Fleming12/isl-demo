aa96f6250519e6a770417e3860f1aaadb90ab780
James Fleming <jamesfleming.sd@gmail.com>
2025-12-26 12:42:26 -0800

    fixed loss output

--- cust_phi.py
+++ cust_phi.py
@@ -423,10 +423,11 @@ class QuarterBlockPhi3Transformer(QuarterBlockPhi3):
                     all_hidden_states += (hidden_states,)
 
         hidden_states = self.norm(hidden_states)
+        all_hidden_states = all_hidden_states[:-1]
 
         # add hidden states from the last decoder layer
-        if output_hidden_states:
-            all_hidden_states += (hidden_states,)
+        # if output_hidden_states:
+        #     all_hidden_states += (hidden_states,)
 
         next_cache = next_decoder_cache if use_cache else None
         if return_legacy_cache:
